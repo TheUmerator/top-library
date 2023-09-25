3a2f819e947d8d523751d6ecff5947cb99e542ba
umerHP <54628763+TheUmerator@users.noreply.github.com>
2023-09-25 12:16:51 +0500

    Add stable book addition to myLibrary

--- script.js
+++ script.js
@@ -22,16 +22,43 @@ function addBookToLibrary() {
 //DIALOG OPENING PROCESS
 
 const addButton = document.querySelector(".add-icon");
-const bookForm = document.querySelector('.add-book');
+const bookDialog = document.querySelector('.add-book');
 
 const bookName = document.querySelector('input[id="book-name"]');
 const addTitle = document.querySelector('.modal-title');
 
+const addForm = document.querySelector('.add-form');
 
 addButton.addEventListener('click', () => {
-    bookForm.showModal();
+    bookDialog.showModal();
 
     bookName.addEventListener("input", e => addTitle.textContent = e.target.value);
 
-}
-)
+    addForm.addEventListener('submit', e => {
+        e.preventDefault();
+
+        const bookInfo = new FormData(addForm);
+        const obj = Object.fromEntries(bookInfo);
+
+
+        if (obj['book-name'] != '')
+            myLibrary.push(obj);
+
+        console.log(myLibrary);
+        addForm.reset();
+        addTitle.textContent='Add Book Name';
+        bookDialog.close();
+
+    })
+})
+/*
+//WHAT TO DO FURTHER ON:
+1. Disable form submission and instead
+   reroute obtained information to an array
+    COMPLETED
+
+2. Clear form data after pressing submit
+
+3. Complete STAGE3 (Adding the Card after the
+    form is submitted with the info)
+*/
