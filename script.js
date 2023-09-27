1cba6c0180f367c6900bccdb8531c90112dfea5b
umerHP <54628763+TheUmerator@users.noreply.github.com>
2023-09-27 07:12:25 +0500

    Successfully get form data to card display

--- script.js
+++ script.js
@@ -29,15 +29,16 @@ const addTitle = document.querySelector('.modal-title');
 
 const addForm = document.querySelector('.add-form');
 
-const AddContainer=document.querySelector('.add-container');
-const siteContainer=document.querySelector('.site-container');
-const bookContainer=document.querySelector('.book-container');
+const AddContainer = document.querySelector('.add-container');
+const siteContainer = document.querySelector('.site-container');
+const bookContainer = document.querySelector('.book-container');
 
-const cardContainer=document.querySelector('.card-container');
-const cardAddButton=addButton.cloneNode(true);
-
-
-addButton.addEventListener('click', () => {
+const cardContainer = document.querySelector('.card-container');
+const cardAddButton = addButton.cloneNode(true);
+let bookCount = 0;
+// cardAddButton
+//addButton
+cardAddButton.addEventListener('click', () => {
     bookDialog.showModal();
 
     bookName.addEventListener("input", e => addTitle.textContent = e.target.value);
@@ -45,43 +46,86 @@ addButton.addEventListener('click', () => {
     addForm.addEventListener('submit', e => {
         e.preventDefault();
 
-        const bookInfo = new FormData(addForm);     //extracts from data to a new FormData Object
-        const obj = Object.fromEntries(bookInfo);   //divides it into key value pairs
+        const bookInfo = new FormData(addForm); //extracts from data to a new FormData Object
+        const obj = Object.fromEntries(bookInfo); //divides it into key value pairs
 
 
-        if (obj['book-name'] != '')
+        if (obj['book-name'] != '') {
+            bookCount++;
+
+
             myLibrary.push(obj);
 
-        console.log(myLibrary);
-        addForm.reset();
-        addTitle.textContent='Add Book Name';
-        bookDialog.close();
+
+            console.log(myLibrary);
+            addForm.reset();
+            addTitle.textContent = 'Add Book Name';
+            bookDialog.close();
 
 
-        AddContainer.style.display='none';      //This clears the + button
-        siteContainer.classList.add('added');   //Adds .added to the classlist of the siteContainer
-        bookContainer.classList.add('visible');   //Adds .added to the classlist of the siteContainer
+            AddContainer.style.display = 'none'; //This clears the + button
+            siteContainer.classList.add('added'); //Adds .added to the classlist of the siteContainer
+            bookContainer.classList.add('visible'); //Adds .added to the classlist of the siteContainer
+
+            const card = document.createElement('div');
+            card.classList.add('card');
+
+            const cardName = document.createElement('h2');
+            cardName.classList.add('name');
+            cardName.textContent=myLibrary[bookCount - 1]['book-name'];
+            card.appendChild(cardName);
+
+
+            const cardAuthor = document.createElement('h3');
+            cardAuthor.classList.add('author');
+            cardAuthor.textContent=myLibrary[bookCount - 1]['book-author'];
+            card.appendChild(cardAuthor);
+
+
+            const cardPages = document.createElement('div');
+            cardPages.classList.add('pages');
+            cardPages.textContent=myLibrary[bookCount - 1]['book-pages'];
+            card.appendChild(cardPages);
+
+
+            const cardStatus = document.createElement('div');
+            cardStatus.classList.add('status');
+
+            card.appendChild(cardStatus);
+
+
+            const cardRemove = document.createElement('button');
+            cardRemove.classList.add('remove');
+
+            card.appendChild(cardRemove);
+
+
+            cardContainer.appendChild(card);
+        }
+
+
 
     })
 })
 
-addEventListener('keydown',(e)=>{
-    if (e.key=='c') {
+addEventListener('keydown', (e) => {
+    if (e.key == 'c') {
         console.log('created')
 
-        const card=document.createElement('div');
+        const card = document.createElement('div');
         card.classList.add('card');
         cardContainer.appendChild(card);
     }
-    if(e.key=='d'){
+    if (e.key == 'd') {
         console.log('deleted')
         cardContainer.removeChild(cardContainer.lastElementChild)
     }
-    if(e.key=='a'){
-        const card=document.createElement('div');
+    if (e.key == 'a') {
+        const card = document.createElement('div');
 
         card.classList.add('card');
-        
+        card.classList.add('add');
+
         card.appendChild(cardAddButton);
         cardAddButton.classList.add('card-icon');
         cardContainer.appendChild(card);
@@ -96,10 +140,15 @@ addEventListener('keydown',(e)=>{
     HTML & CSS:
 -(DONE) Lay out book-container layout in headers and body format
 -(DONE) Create first book block layout with sample text
--Create the additional book layout
+-(DONE) Create the additional book layout
 -(DONE) Create a grid with both book layouts with book-container body as parent
--Set grid up so that it starts wrapping after a certain book amount
+-(DONE) Set grid up so that it starts wrapping after a certain book amount
 
     JS:
--We'll figure it out once we get the HTML & CSS portion nailed.
+-Streamline the code so that its more readable
+-We want the cards to reflect on the READ/UNREAD status, the selected 
+ choice should select that button by default
+-We also want to make the READ/UNREAD an actual button the user can click
+ so that it actually changes the database on the back end. Speaking of,
+-We need the array of myLibrary to keep track of what we're doing.
 */
